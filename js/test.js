12bac4b0803590b88539f0baa3de3d124ee09b7d
poojakatkar1807 <poojakatkar1807@gmail.com>
2026-05-18 15:20:51 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,5 +1,23 @@
-console.log("My Name is Radhika", "I live in Pune ", "My age is 30")
- myname = "Pooja"
- age = 20
- city = "Mumbai"
- console.log(`My Name is ${myname} I live in ${city} My age is ${age}`);
+// console.log("My Name is Radhika", "I live in Pune ", "My age is 30")
+//  myname = "Pooja"
+//  age = 20
+//  city = "Mumbai"
+//  console.log(`My Name is ${myname} I live in ${city} My age is ${age}`);
+
+
+//  let a = 10;
+// console.log(a);
+// {
+//   let a = 20;
+//   console.log(a);
+// }
+// console.log(a);
+
+
+let a = 10;
+
+{
+  let a = 20;
+  console.log(a)
+}
+console.log(a)
